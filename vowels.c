8b0300d8ba94cd84ad16555a226210bb8b2314c6
Joel Roberts J <53214646+JoelChar@users.noreply.github.com>
2019-07-24 15:50:39 +0530

    Update vowels.c

--- vowels.c
+++ vowels.c
@@ -5,7 +5,7 @@ int main()
   scanf("%c",&ch);
   
   if((ch >= 'a'&& ch<='z')||(ch >='A' && ch >='Z')){
-    if(ch = 'a' || ch = 'A' || ch = 'e' || ch = 'E' || ch = 'I'||ch='i'||ch='o'||ch='O'||ch='U'||ch='u')
+    if(ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'I' || ch == 'i' || ch == 'o' || ch == 'O' || ch == 'U' || ch == 'u')
       printf("%c is a vowel\n",ch);
      else
       printf("%c is a consonant",ch);
